30a2dcd981cf8858e1c5d06e353751c5f9b6799c
Rodrigo Tobar <rtobar@icrar.org>
2016-07-27 10:10:43 +0800

    pyprofit 0.8.6
    This is for real
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ pyprofit_ext = Extension('pyprofit',
 
 setup(
       name='pyprofit',
-      version='0.8.5',
+      version='0.8.6',
       description='Libprofit wrapper for Python',
       author='Rodrigo Tobar',
       author_email='rtobar@icrar.org',
