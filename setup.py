54c2774afb191a355f89d5e911524e00386cbcd6
Rodrigo Tobar <rtobar@icrar.org>
2016-08-31 13:13:49 +0800

    pyprofit 0.15.2
    Based on libprofit 0.15.2 which brings a compilation fix for old gcc compilers.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ pyprofit_ext = Extension('pyprofit',
 
 setup(
       name='pyprofit',
-      version='0.15',
+      version='0.15.2',
       description='Libprofit wrapper for Python',
       author='Rodrigo Tobar',
       author_email='rtobar@icrar.org',
