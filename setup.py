584c8a008ea9343f4467d10a285e7de0d952e122
Rodrigo Tobar <rtobar@icrar.org>
2016-08-31 17:13:10 +0800

    pyprofit 0.15.4
    Based on libprofit 0.15.3 which brings the execution times down.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ pyprofit_ext = Extension('pyprofit',
 
 setup(
       name='pyprofit',
-      version='0.15.3',
+      version='0.15.4',
       description='Libprofit wrapper for Python',
       author='Rodrigo Tobar',
       author_email='rtobar@icrar.org',
