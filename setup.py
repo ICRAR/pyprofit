8885ee289182c8b6ace3938be48f782ae821e126
Rodrigo Tobar <rtobar@icrar.org>
2020-06-12 10:10:11 +0800

    Signal compatibility with python 3.8 and 3.9
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- setup.py
+++ setup.py
@@ -351,6 +351,8 @@ setup(
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
           "Topic :: Scientific/Engineering :: Astronomy"
       ],
       ext_modules = [pyprofit_ext],
